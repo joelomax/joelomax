c30a135ecdb9964cc1b35978cac37c43b97bbb62
Adam Deeley <adam.deeley@oneiota.co.uk>
2019-11-02 16:20:32 +0000

    Refactor code and fix turn order veriable assignment.:

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -10,8 +10,9 @@ def display_board(block):
     print("    |     |    ")
     print(block[1]+"   |   "+block[2]+" |   "+block[3])
     print("    |     |    ")
- 
-block = ["-"," "," "," "," "," "," "," "," "," "]
+
+
+block = ["-", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 select_menu = 0
 while select_menu == 0:
@@ -25,7 +26,7 @@ while select_menu == 0:
         break
     elif select_player == "X" or select_player == "x":
         player1_piece = "X"
-        player2_piece = "O" 
+        player2_piece = "O"
         select_menu = 1
         break
     else:
@@ -36,7 +37,7 @@ display_board(block)
 print("input 1 to 9 on the num pad to select which square to enter")
 
 game_over = 0
-turn_order=1
+turn_order = 1
 while game_over == 0:
     if " " not in block:
         game_over == 1
@@ -45,12 +46,12 @@ while game_over == 0:
         try:
             player1_entry = int(input("player 1: where will you go?  "))
         except:
-            tf=False
-            while tf==False:
-                player1_entry=input("Please enter a valid number!")
+            tf = False
+            while tf == False:
+                player1_entry = input("Please enter a valid number!")
                 try:
-                    if int(player1_entry) in range(0,10):
-                        tf=True
+                    if int(player1_entry) in range(0, 10):
+                        tf = True
                 except:
                     continue
 
@@ -62,19 +63,19 @@ while game_over == 0:
         else:
             block[int(player1_entry)] = player1_piece
             print(len(block))
-            turn_order=0
+            turn_order = 0
             display_board(block)
     elif turn_order == 0:
 
         try:
             player2_entry = int(input("player 2: where will you go?  "))
         except:
-            tf=False
-            while tf==False:
-                player2_entry=input("Please enter a valid number!")
+            tf = False
+            while tf == False:
+                player2_entry = input("Please enter a valid number!")
                 try:
-                    if int(player2_entry) in range(0,10):
-                        tf=True
+                    if int(player2_entry) in range(0, 10):
+                        tf = True
                 except:
                     continue
 
@@ -86,7 +87,7 @@ while game_over == 0:
         else:
             block[int(player2_entry)] = player2_piece
             print(len(block))
-            turn_order=0
+            turn_order = 1
             display_board(block)
 
 print("game over!")
