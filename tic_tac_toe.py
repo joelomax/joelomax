c1c6f0c4a75ae6a8f7a7c6c8518112e32ae9fba1
Adam Deeley <adam.deeley@oneiota.co.uk>
2019-11-02 20:06:33 +0000

    win state arrays

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -11,41 +11,45 @@ def display_board(block):
     print(block[1]+"   |   "+block[2]+" |   "+block[3])
     print("    |     |    ")
 
+winstates = [
+	[1,2,3],
+	[4,5,6],
+	[7,8,9],
+	[1,4,7],
+	[2,5,8],
+	[3,6,9],
+	[1,5,9],
+	[3,5,7]
+]
 
-block = ["-", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+block = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 player_pieces = {
     'player1': '',
     'player2': ''
 }
 
-select_menu = 0
-while select_menu == 0:
-    select_player = input("player 1: what do you want to be, type X or O   ")
-    if select_player == "0":
-        print("please use O, not 0")
-    elif select_player == "O":
-        player_pieces['player1'] = "O"
-        player_pieces['player2'] = "X"
-        select_menu = 1
-        break
-    elif select_player == "X" or select_player == "x":
-        player_pieces['player1'] = "X"
-        player_pieces['player2'] = "O"
-        select_menu = 1
-        break
-    else:
-        print("please select a valid player marker")
+while True:
+    counter_selection = input(
+        "player 1: what do you want to be, type X or O   ")
+    if (counter_selection != 'X' or counter_selection != 'O'):
+        print('Please type X or O')
+        continue
+    player_pieces['player1'] = counter_selection
+    player_pieces['player2'] = "X" if counter_selection == "O" else "O"
+    break
 
 display_board(block)
 
 print("input 1 to 9 on the num pad to select which square to enter")
 
+
 def getUserInput(playerNumber):
     validInput = False
     while validInput == False:
         try:
-            player1_entry = int(input("player {}: where will you go?  ".format(playerNumber)))
-            if int(player1_entry) in range(0, 10):
+            player1_entry = int(
+                input("player {}: where will you go?  ".format(playerNumber)))
+            if int(player1_entry) in range(1, 10):
                 validInput = True
             else:
                 print("Please enter a valid number within 1 to 9!")
@@ -54,26 +58,32 @@ def getUserInput(playerNumber):
 
     return player1_entry
 
+
 def isValidMove(player_entry):
-    if block[int(player_entry)] != " ":
+    if block[int(player_entry) -1] != " ":
         print("space is not vacant!  ")
         return False
     else:
         return True
 
+
 def endGame():
     if " " not in block:
         return True
 
+def decideWinner():
+
+
 turn_order = 0
 while True:
-    turn_order = 1 - turn_order #Nice!
+    turn_order = 1 - turn_order  # Nice!
     if endGame():
         break
     player_entry = getUserInput(turn_order)
     if (isValidMove(player_entry)):
-        block[int(player_entry)] = player_pieces['player' + turn_order]
+        block[int(player_entry)] = player_pieces['player' + str(turn_order + 1)]
         print(len(block))
         display_board(block)
 
+decideWinner()
 print("game over!")
